6747fab8b6af7fefa4a5c438881cf8fda765c517
daniel.carmona <daniel.carmona@stormharvester.com>
2022-04-09 12:32:14 +0100

    Add chain rule for 3 method

--- generic/operations.py
+++ generic/operations.py
@@ -11,11 +11,10 @@ Array_Function = Callable[[ndarray], ndarray]
 Chain = List[Array_Function]
 
 
-def deriv(
-        func: Array_Function,
-        input_: ndarray,
-        delta: float = 0.001
-        ) -> ndarray:
+def deriv(func: Array_Function,
+          input_: ndarray,
+          delta: float = 0.001
+          ) -> ndarray:
     """
     Evaluates the derivative of a function at every element in the input_ array
     :param func: function to evaluate
@@ -27,10 +26,9 @@ def deriv(
     return (func(input_ + delta) - func(input_ - delta)) / (2 * delta)
 
 
-def chain_deriv_2(
-        chain: Chain,
-        input_range: ndarray
-        ) -> ndarray:
+def chain_deriv_2(chain: Chain,
+                  input_range: ndarray
+                  ) -> ndarray:
     """
     Uses teh chain rule to compute the derivative of two nested functions:
     :param chain: chain of functions
@@ -57,10 +55,35 @@ def chain_deriv_2(
     return deriv_f2_du * deriv_f1_dx
 
 
-def chain_length_2(
-        chain: Chain,
-        x: ndarray
-        ) -> ndarray:
+def chain_deriv_3(chain: Chain,
+                  input_range: ndarray
+                  ) -> ndarray:
+    """
+    Uses the chain rule to compute teh derivative of three nested functions:
+    (f3(f2(f1)))' = f3'(f2(f1(x))) * f2'(f1(x)) * f1'(x)
+    :param chain: chain of functions, first in the list being f1
+    :param input_range: The point where to compute de derivative
+    :return: ndarray with the values of the derivative in the input points
+    """
+    assert len(chain) == 3, "This function requires. 'Chain' objects to have length 3"
+
+    f1 = chain[0]
+    f2 = chain[1]
+    f3 = chain[3]
+
+    f1_of_x = f1(input_range)
+    f2_of_f1_of_x = f2(f1_of_x)
+
+    df3du = deriv(f3, f2_of_f1_of_x)
+    df2du = deriv(f2, f1_of_x)
+    df1dx = deriv(f1, input_range)
+
+    return df1dx * df2du * df3du
+
+
+def chain_length_2(chain: Chain,
+                   x: ndarray
+                   ) -> ndarray:
     """
     Evaluates two functions in a row, in a "Chain". (Applying in the list order)
     :param chain: the chain of functions
@@ -78,9 +101,8 @@ def chain_length_2(
         return None
 
 
-def square(
-        x: ndarray
-        ) -> ndarray:
+def square(x: ndarray
+           ) -> ndarray:
     """
     Square each element in the input array
     :param x: numpy array
@@ -89,9 +111,8 @@ def square(
     return np.power(x, 2)
 
 
-def leaky_relu(
-        x: ndarray
-        ) -> ndarray:
+def leaky_relu(x: ndarray
+               ) -> ndarray:
     """
     Apply "Leaky ReLU function to each element in the input ndarray.
     :param x: input ndarray
@@ -100,9 +121,8 @@ def leaky_relu(
     return np.maximum(0.2 * x, x)
 
 
-def sigmoid(
-        x: ndarray,
-        ) -> ndarray:
+def sigmoid(x: ndarray,
+            ) -> ndarray:
     """
     Apply the sigmoid function to each element in the input ndarray
     :param x: input ndarray
